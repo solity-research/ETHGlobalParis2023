d7a3b3e5a867f72b05024d9214f19c02a22c67f9
cbayraktaroglu <56252393+cbayraktaroglu@users.noreply.github.com>
2023-07-23 00:18:38 +0200

    Update CreditScore.sol

--- frontend/contracts/CreditScore.sol
+++ frontend/contracts/CreditScore.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
 import "@semaphore-protocol/contracts/interfaces/ISemaphore.sol";
@@ -7,16 +8,18 @@ contract CreditScore {
 
     uint256 public groupId;
     uint256 private externalNul = 22252025330403739761823611755537792214391358329992882809179600052859407695872;
-    
+    uint256[] public commitments;
+
     constructor(address semaphoreAddress, uint256 _groupId) {
         semaphore = ISemaphore(semaphoreAddress);
         groupId = _groupId;
 
-        semaphore.createGroup(groupId, 16, address(this));
+        semaphore.createGroup(groupId, 16, address(this), 48 hours);
     }
 
     function joinGroup(uint256 identityCommitment) external {
         semaphore.addMember(groupId, identityCommitment);
+        commitments.push(identityCommitment);
     }
 
     function sendCreditScore(
