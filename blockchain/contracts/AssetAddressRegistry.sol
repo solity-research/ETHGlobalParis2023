442535298eeb2d1aaa6b3ce0b7d1ce1e15a06153
cbayraktaroglu <56252393+cbayraktaroglu@users.noreply.github.com>
2023-07-22 09:33:22 +0200

    Testing

---
+++ blockchain/contracts/AssetAddressRegistry.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract AssetAddressRegistry {
+
+  address private owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+
+}
