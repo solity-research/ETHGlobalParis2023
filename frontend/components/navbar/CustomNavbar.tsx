f10c9c948fd67ca21dbdb9a7914273a00483014b
cemdenizsel <cem.denizsel@gmail.com>
2023-07-22 16:24:04 +0200

    semaphore update.

--- frontend/components/navbar/CustomNavbar.tsx
+++ frontend/components/navbar/CustomNavbar.tsx
@@ -39,11 +39,11 @@ const CustomNavbar = () => {
                     </Link>
                 </nav>
                 <div className="hidden w-full lg:flex md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
+                    <div className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        
                             {!isLogin() && <button onClick={handleLogin} type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Login</button>}
-                        </li>
-                    </ul>
+                        
+                    </div>
                 </div>
             </div>
         </nav>
--- frontend/index.d.ts
+++ frontend/index.d.ts
@@ -2,6 +2,10 @@ declare namespace NodeJS{
     interface ProcessEnv {
         API_KEY:string
         WORD_COIN: string
+        ALCHEMY_API_KEY:string
+        DEFAULT_NETWORK:string
+        ETHEREUM_PRIVATE_KEY:string
+        PROOF_STORE:string
+        CREDITSCORE_CONTRACT_ADDRESS: string
     }
 }
-  
