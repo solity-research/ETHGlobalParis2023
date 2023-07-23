b130cac8806f9de3b0675d34abfe91e0dd21a690
cemdenizsel <cem.denizsel@gmail.com>
2023-07-23 04:39:15 +0200

    main page restructure.

--- frontend/components/hero/Hero.tsx
+++ frontend/components/hero/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
             <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply"></div>
             <div className="relative flex flex-col items-center p-4 sm:max-w-xl">
                 <p className="mb-4 text-center text-lg text-indigo-200 sm:text-xl md:mb-8">Very proud to introduce</p>
-                <h1 className="mb-8 text-center text-4xl font-bold text-white sm:text-5xl md:mb-12 md:text-6xl">Revolutionary way to build the web</h1>
+                <h1 className="mb-8 text-center text-4xl font-bold text-white sm:text-5xl md:mb-12 md:text-6xl">Secure Private Transparent Score</h1>
 
                 <div className="flex w-full flex-col gap-2.5 sm:flex-row sm:justify-center">
                     <a href="#" className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">Start now</a>
--- frontend/components/navbar/CustomNavbar.tsx
+++ frontend/components/navbar/CustomNavbar.tsx
@@ -30,9 +30,8 @@ const CustomNavbar = () => {
     return (
         <nav className=" border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" className="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <a href="" className="flex items-center">
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CréditDécentrale</span>
                 </a>
                 <nav className="hidden gap-12 lg:flex">
                     <Link href="/credit">
